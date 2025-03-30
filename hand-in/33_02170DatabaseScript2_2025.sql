79b581da862e757a6f5afc57f05e28567dbe01cc
spezzu <s225115@dtu.dk>
2025-03-30 11:13:54 +0200

    Updated procedure

--- hand-in/33_02170DatabaseScript2_2025.sql
+++ hand-in/33_02170DatabaseScript2_2025.sql
@@ -6,27 +6,55 @@ DROP TRIGGER IF EXISTS update_stock_on_return;
 -- FUNCTION
 
 -- PROCEDURE
+DROP PROCEDURE IF EXISTS register_borrow;
+DROP TRIGGER IF EXISTS update_stock_on_return;
+
 DELIMITER //
 CREATE PROCEDURE register_borrow(
 	IN in_book_id INT,
-	IN in_client_id INT)
+	IN in_client_id INT,
+    OUT out_message VARCHAR(30),
+    OUT out_code INT) -- -1 book not found, 0 success, 1 not available
 BEGIN
-	DECLARE local_stock INT DEFAULT 0;
+	-- variable declaration
+	DECLARE local_stock INT;
+    SET local_stock = (SELECT stock FROM Book WHERE book_id = in_book_id);
 	
-	SELECT stock INTO local_stock FROM Book WHERE book_id = in_book_id;
-
-	IF local_stock>0 THEN
-	INSERT INTO Borrows (client_id, book_id, due_date, return_date)
+    IF local_stock IS NULL THEN
+		SET out_message = 'Book ID could not be found.';
+        SET out_code = -1;
+	ELSEIF local_stock>0 THEN
+		INSERT INTO Borrows (client_id, book_id, due_date, return_date)
         VALUES (in_client_id, in_book_id, CURDATE() + INTERVAL 30 DAY, null);
-	UPDATE Book SET stock = stock-1 WHERE book_id = in_book_id;
+		UPDATE Book SET stock = stock-1 WHERE book_id = in_book_id;
+        SET out_message = 'Book borrowing was successful.';
+        SET out_code = 0;
 	ELSE
-	SIGNAL SQLSTATE '45000'
-	SET MYSQL_ERRNO= 10001, MESSAGE_TEXT = 'Book is out of stock.';
+        SET out_message = 'Book is out of stock.';
+        SET out_code = 1;
 	END IF;
 END //
 DELIMITER ;
 
--- TRIGGER
+-- Tables before procedure
+SELECT * FROM Borrows; 
+SELECT book_id, book_title, section, stock FROM Book WHERE section = 'Antiques';
+
+-- Apply procedure
+CALL register_borrow(19, 1, @out_m, @out_c); -- 19 has only one copy; should drop to 0
+
+SELECT @out_m, @out_c;
+SELECT * FROM Borrows;
+SELECT book_id, book_title, section, stock FROM Book WHERE section = 'Antiques';
+
+CALL register_borrow(19, 1, @out_m, @out_c); -- 19 has zero copies; should return code 1
+
+SELECT @out_m, @out_c;
+
+CALL register_borrow(100,1,@out_m ,@out_c); -- 100 does not exist; should return code -1
+SELECT @out_m, @out_c;
+
+-- Trigger
 DELIMITER //
 CREATE TRIGGER update_stock_on_return
 AFTER UPDATE ON Borrows
@@ -39,6 +67,12 @@ BEGIN
     END IF;
 END//
 DELIMITER ;
+
+UPDATE Borrows SET return_date = CURDATE() WHERE book_id = 19 AND client_id = 1;
+
+-- See result of trigger on tables
+SELECT * FROM Borrows;
+SELECT book_id, book_title, section, stock FROM Book WHERE section = 'Antiques';
 
 -- DELETE 
 DELETE FROM Borrows
